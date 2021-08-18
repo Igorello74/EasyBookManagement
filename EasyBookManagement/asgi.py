5b0fc72849d832b862e9f5aa37d4ea756148386b
Igorello74 <sidneff.igor@ya.ru>
2021-08-19 00:08:01 +0500

    init django stuff

---
+++ EasyBookManagement/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for EasyBookManagement project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyBookManagement.settings')
+
+application = get_asgi_application()
