e00f03ebb6f44d17c1329a3fe38e8d64f93c64eb
Igorello74 <sidneff.igor@ya.ru>
2021-08-24 14:54:57 +0500

    edit booksRecords.models

--- booksRecords/models.py
+++ booksRecords/models.py
@@ -2,20 +2,44 @@ from django.db import models
 
 # Create your models here.
 
-class BookRecord(models.Model):
+class Book(models.Model):
     """
     Формуляр книги. К нему могут относится множество экземпляров.
     """
-    id = models.BigAutoField(
-      unique=True, primary_key=True, verbose_name="идентификационный номер",
-      help_text="Предпочтительно, чтобы этот номер соответствовал ISBN-коду книги (если он есть)"
+
+    # Main fields
+    isbn = models.BigIntegerField(
+        verbose_name="ISBN номер", db_index=True, null=True, blank=True,
+        help_text="Обязательно заполнять при наличии такового.\n"
+        "Чтобы ускорить процесс, можно отсканировать штрихкод с ISBN-ом "
+        "сканером (обычно располагается на обратной стороне книги)"
     )
-    name = models.CharField(verbose_name="название")
-    authors = models.CharField(
-      verbose_name="автор(-ы)", 
-      help_text="Авторы книги (сокращённо) через запятую", 
-      max_length=200
+    name = models.CharField(verbose_name="название", max_length=65,)
+    authors = models.TextField(
+        verbose_name="автор(-ы)", 
+        help_text="Авторы книги (сокращённо) через запятую", 
     )
+    inventory_number = models.BigIntegerField(
+        verbose_name="инвентарный номер"
+    )
+    
+    # Publication info
     year = models.SmallIntegerField(verbose_name="год издания")
-    publisher = models.CharField(verbose_name="издательство")
-    # TODO
+    publisher = models.CharField(
+        verbose_name="издательство", max_length=20)
+    edition = models.SmallIntegerField(verbose_name='номер издания')
+    city = models.CharField(
+        max_length=30, verbose_name="город издания",
+        help_text='город издания книги, без "г. "'
+    )
+
+    # Educational info
+    grade = models.CharField(
+        max_length=5, verbose_name="класс", blank=True,
+        help_text="Может быть диапазоном, к примеру: \"7-9\""
+    )
+    subject = models.CharField(
+        verbose_name="предмет", max_length=20, blank=True,
+        help_text='Например: "математика", "русский язык", '
+        '"математика углублённая"'
+    )
