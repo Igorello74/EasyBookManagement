e839b9a34636c815e221ef0538ba15050cfe84ba
Igorello74 <sidneff.igor@ya.ru>
2022-06-05 01:20:11 +0500

    fix #4

--- readersRecords/admin.py
+++ readersRecords/admin.py
@@ -11,3 +11,13 @@ class ReaderAdmin(admin.ModelAdmin):
     list_display = ("name", "role", "group", get_books)
     search_fields = ("name", 'group')
     autocomplete_fields = ['books']
+
+    class Media:
+        js = (
+          'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/scripts/choices.min.js',
+        )
+        css = {
+            'all':(
+              'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css',
+            )
+        }
