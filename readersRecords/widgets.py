92baf0287c3e48b0aa7603ad6870fc22a84dba9b
Igorello74 <sidneff.igor@ya.ru>
2022-06-05 16:18:20 +0500

    move assets definitions to a dedicatied file; fix #4

---
+++ readersRecords/widgets.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class CalendarWidget(forms.SelectMultiple):
+    class Media:
+        js = (
+          'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/scripts/choices.min.js',
+        )
+        css = {
+            'all':(
+              'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css',
+            )
+        }
