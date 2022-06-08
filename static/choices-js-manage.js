7b5754cdc29670d5b1067bbeca73bd947ffa0366
Igorello74 <sidneff.igor@ya.ru>
2022-06-08 21:59:11 +0500

    fix #6

--- static/choices-js-manage.js
+++ static/choices-js-manage.js
@@ -1,7 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+function isTwiceOrMore(array, item) {
+    ind1 = array.indexOf(item);
+    if (ind1 != -1) {
+        ind2 = array.indexOf(item, ind1+1)
+
+        if (ind2 != -1) {return true;}
+    }
+    return false;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     var instances = document.querySelectorAll('[choicesjs]');
     for (i = 0; i < instances.length; ++i) {
         var element = instances[i];
-        new Choices(element, {position: 'bottom', removeItemButton: true});
+        const ch = new Choices(element, { position: 'bottom', removeItemButton: true });
+        
+        element.addEventListener(
+            'addItem',
+            (event)=>{
+                new_item = event.detail.value;
+                if (isTwiceOrMore(ch.getValue(true), new_item)) {
+                    ch.removeActiveItemsByValue(new_item);
+                }
+            }
+        )
     }
 })
