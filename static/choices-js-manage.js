5af8d70c3491e0a4aa245c8bd1e8b61c7b45ec60
Igorello74 <sidneff.igor@ya.ru>
2022-06-09 00:36:17 +0500

    make some minor style fixes

--- static/choices-js-manage.js
+++ static/choices-js-manage.js
@@ -1,9 +1,9 @@
 function isTwiceOrMore(array, item) {
     ind1 = array.indexOf(item);
     if (ind1 != -1) {
-        ind2 = array.indexOf(item, ind1+1)
+        ind2 = array.indexOf(item, ind1 + 1)
 
-        if (ind2 != -1) {return true;}
+        if (ind2 != -1) { return true; }
     }
     return false;
 }
@@ -13,49 +13,48 @@ function sendMessage(messageList, messageClass, messageContent) {
     li.className = messageClass;
     li.innerHTML = messageContent;
     messageList.append(li);
+    li.scrollIntoView();
 }
 
 function sendRemovalMessage(messageList, bookInstance) {
-    sendMessage(messageList, 'error', `Книга с кодом ${bookInstance} была принята у читателя`);
+    sendMessage(messageList, 'error', `Книга #${bookInstance} была удалена`);
 }
 function sendAdditionMessage(messageList, bookInstance) {
-    sendMessage(messageList, 'success', `Книга с кодом ${bookInstance} была выдана читателю`);
+    sendMessage(messageList, 'success', `Книга #${bookInstance} была добавлена`);
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
     messageList = document.createElement("ul");
     messageList.className = "messagelist";
-    let content = document.querySelector("div.content");
-    content.prepend(messageList);
+    let outer = document.querySelector("nav#nav-sidebar");
+    outer.append(messageList);
 
 
-    var instances = document.querySelectorAll('[choicesjs]');
-    for (i = 0; i < instances.length; ++i) {
-        var element = instances[i];
-        const ch = new Choices(element, { position: 'bottom', removeItemButton: true });
-        
-        element.addEventListener(
-            'addItem',
-            (event)=>{
-                new_item = event.detail.value;
-                if (isTwiceOrMore(ch.getValue(true), new_item)) {
-                    ch.removeActiveItemsByValue(new_item);
-                }
-                else {
-                    sendAdditionMessage(messageList, new_item);
-                }
+    var element = document.querySelector('[choicesjs]');
+    const ch = new Choices(element, { position: 'bottom', removeItemButton: true });
+
+    element.addEventListener(
+        'addItem',
+        (event) => {
+            new_item = event.detail.value;
+            if (isTwiceOrMore(ch.getValue(true), new_item)) {
+                ch.removeActiveItemsByValue(new_item);
             }
-        )
-
-        element.addEventListener(
-            'change',
-            (event) => {
-                item = event.detail.value;
-                if (ch.getValue(true).indexOf(item) == -1) {
-                    sendRemovalMessage(messageList, item);
-                }
+            else {
+                sendAdditionMessage(messageList, new_item);
             }
-        )
-    }
+        }
+    )
+
+    element.addEventListener(
+        'change',
+        (event) => {
+            item = event.detail.value;
+            if (ch.getValue(true).indexOf(item) == -1) {
+                sendRemovalMessage(messageList, item);
+            }
+        }
+    )
+
 })
