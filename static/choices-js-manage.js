c0da81d1acabd1cf37ec8367515cb2e9451da5b1
Igorello74 <sidneff.igor@ya.ru>
2022-06-08 21:15:24 +0500

    add remove button to choices.js widget

--- static/choices-js-manage.js
+++ static/choices-js-manage.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = document.querySelectorAll('[choicesjs]');
     for (i = 0; i < instances.length; ++i) {
         var element = instances[i];
-        new Choices(element, {position: 'bottom'});
+        new Choices(element, {position: 'bottom', removeItemButton: true});
     }
 })
