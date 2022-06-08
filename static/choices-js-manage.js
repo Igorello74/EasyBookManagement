307fe6c84b5c32f0ab52a344cb3a9a2d488ed19a
Igorello74 <sidneff.igor@ya.ru>
2022-06-09 00:03:42 +0500

    implement short logging on reader's change page; #8

--- static/choices-js-manage.js
+++ static/choices-js-manage.js
@@ -8,7 +8,28 @@ function isTwiceOrMore(array, item) {
     return false;
 }
 
+function sendMessage(messageList, messageClass, messageContent) {
+    let li = document.createElement('li');
+    li.className = messageClass;
+    li.innerHTML = messageContent;
+    messageList.append(li);
+}
+
+function sendRemovalMessage(messageList, bookInstance) {
+    sendMessage(messageList, 'error', `Книга с кодом ${bookInstance} была принята у читателя`);
+}
+function sendAdditionMessage(messageList, bookInstance) {
+    sendMessage(messageList, 'success', `Книга с кодом ${bookInstance} была выдана читателю`);
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
+    messageList = document.createElement("ul");
+    messageList.className = "messagelist";
+    let content = document.querySelector("div.content");
+    content.prepend(messageList);
+
+
     var instances = document.querySelectorAll('[choicesjs]');
     for (i = 0; i < instances.length; ++i) {
         var element = instances[i];
@@ -20,6 +41,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 new_item = event.detail.value;
                 if (isTwiceOrMore(ch.getValue(true), new_item)) {
                     ch.removeActiveItemsByValue(new_item);
+                }
+                else {
+                    sendAdditionMessage(messageList, new_item);
+                }
+            }
+        )
+
+        element.addEventListener(
+            'change',
+            (event) => {
+                item = event.detail.value;
+                if (ch.getValue(true).indexOf(item) == -1) {
+                    sendRemovalMessage(messageList, item);
                 }
             }
         )
