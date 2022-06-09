525e7e3bcb8b0a2c1e57f6cfb25216d4a4008b88
Igorello74 <sidneff.igor@ya.ru>
2022-06-09 13:03:47 +0500

    add get_bookInstance_info view to booksRecords

--- booksRecords/views.py
+++ booksRecords/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import BookInstance
+
+
+def get_bookInstance_info(request, id):
+    obj = get_object_or_404(BookInstance, pk=id)
+
+    return JsonResponse({
+        'name': obj.book.name,
+        'authors': obj.book.authors,
+        'subject': obj.book.subject or None,
+        'grade': obj.book.grade or None
+    })
