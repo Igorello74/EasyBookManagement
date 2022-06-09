b011ea8549667b1d463d8ed9ef354a67f0329d35
Igorello74 <sidneff.igor@ya.ru>
2022-06-09 14:11:32 +0500

    fix unicode problems with get_bookInstance_info

--- booksRecords/views.py
+++ booksRecords/views.py
@@ -8,8 +8,9 @@ def get_bookInstance_info(request, id):
     obj = get_object_or_404(BookInstance, pk=id)
 
     return JsonResponse({
+        'id': id,
         'name': obj.book.name,
         'authors': obj.book.authors,
         'subject': obj.book.subject or None,
         'grade': obj.book.grade or None
-    })
+    }, json_dumps_params={'ensure_ascii': False})
