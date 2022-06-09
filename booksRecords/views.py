3d9ab7878c0a3c456ef7cca77c9e8f7d1a399b51
Igorello74 <sidneff.igor@ya.ru>
2022-06-09 14:43:19 +0500

    add url to the response of get_bookInstance_info

--- booksRecords/views.py
+++ booksRecords/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 from .models import BookInstance
 
@@ -12,5 +13,6 @@ def get_bookInstance_info(request, id):
         'name': obj.book.name,
         'authors': obj.book.authors,
         'subject': obj.book.subject or None,
-        'grade': obj.book.grade or None
+        'grade': obj.book.grade or None,
+        'admin_url': reverse("admin:booksRecords_bookinstance_change", args=(id,))
     }, json_dumps_params={'ensure_ascii': False})
