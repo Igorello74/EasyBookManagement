b5acdf79c046c8720b4815023c9481be10a5fb5a
Igorello74 <sidneff.igor@ya.ru>
2022-06-10 20:06:04 +0500

    add fixes to choices-js-widget.js

--- static/choices-js-widget.js
+++ static/choices-js-widget.js
@@ -1,25 +1,25 @@
 $ = django.jQuery;
 
 function isTwiceOrMore(array, item) {
-    ind1 = array.indexOf(item);
+    let ind1 = array.indexOf(item);
     if (ind1 != -1) {
-        ind2 = array.indexOf(item, ind1 + 1)
+        let ind2 = array.indexOf(item, ind1 + 1)
 
-        if (ind2 != -1) { return true; }
+        if (ind2 != -1) return true;
     }
     return false;
 }
 
 function createMessage(messageList, messageClass, messageContent) {
-    let li = $(`<li></li>`).html(messageContent).addClass(messageClass).appendTo(messageList);
-    li[0].scrollIntoView();
-    return li;
+    let message = $(`<li></li>`).html(messageContent).addClass(messageClass).appendTo(messageList);
+    message[0].scrollIntoView();
+    return message;
 }
 
 function editItemLabel(editedItem, newLabel, choicesInstance) {
     let items = choicesInstance._currentState.items;
 
-    items = $.grep(items, (x) => { return x.value == editedItem && x.active });
+    items = $.grep(items, x => x.value == editedItem && x.active);
     if (items.length) {
         items[0].label = newLabel;
         choicesInstance._renderItems();
@@ -44,28 +44,28 @@ function updateMessageInfo(id, messageELement, choicesInstance, editItem = false
         })
         .fail(() => {
             messageELement
-              .html(`Некорректный код <span class="messagelist__book-id messagelist__book-id--wrong">#${id}</span>`)
-              .addClass("warning").removeClass("success error");
+                .html(`Некорректный код <span class="messagelist__book-id messagelist__book-id--wrong">#${id}</span>`)
+                .addClass("warning").removeClass("success error");
             choicesInstance.removeActiveItemsByValue(id);
         })
 }
 
 function createAdditionMessage(id, messageList, choicesInstance) {
-    obj = createMessage(
+    let messageObj = createMessage(
         messageList,
         'success',
         `Книга <a class="messagelist__book-id" href="#">#${id}</a> была добавлена`
     );
-    updateMessageInfo(id, obj, choicesInstance, true);
+    updateMessageInfo(id, messageObj, choicesInstance, true);
 }
 
 function createRemovalMessage(id, messageList, choicesInstance) {
-    obj = createMessage(
+    let messageObj = createMessage(
         messageList,
         'error',
         `Книга <a class="messagelist__book-id" href="#">#${id}</a> была удалена`
-        );
-    updateMessageInfo(id, obj, choicesInstance);
+    );
+    updateMessageInfo(id, messageObj, choicesInstance);
 }
 
 
@@ -75,10 +75,10 @@ $(function () {
     $("nav#nav-sidebar").append(messageList);
 
     let choicesElement = $('.choicesjs');
-    choices = new Choices(choicesElement[0], { position: 'bottom', removeItemButton: true });
+    let choices = new Choices(choicesElement[0], { position: 'bottom', removeItemButton: true });
 
     choicesElement.on('addItem', (event) => {
-        new_item = event.detail.value;
+        let new_item = event.detail.value;
         if (isTwiceOrMore(choices.getValue(true), new_item)) {
             choices.removeActiveItemsByValue(new_item);
         }
@@ -88,7 +88,7 @@ $(function () {
     })
 
     choicesElement.on('change', (event) => {
-        item = event.detail.value;
+        let item = event.detail.value;
         if (choices.getValue(true).indexOf(item) == -1) {
             createRemovalMessage(item, messageList, choices);
         }
