60f3b15987a13cc427b52669cb93c9e2072ed415
Igorello74 <sidneff.igor@ya.ru>
2022-07-13 23:24:31 +0500

    change books repr on Reader list page

--- readersRecords/admin.py
+++ readersRecords/admin.py
@@ -9,7 +9,7 @@ from .widgets import ChoicesjsTextWidget
 @admin.register(Reader)
 class ReaderAdmin(admin.ModelAdmin):
     def get_books(self):
-        return ', '.join(str(i) for i in self.books.all())
+        return ' 🔷 '.join(str(i.book.name) for i in self.books.all())
     get_books.short_description = "книги"
 
     list_display = ("name", "role", "group", get_books)
