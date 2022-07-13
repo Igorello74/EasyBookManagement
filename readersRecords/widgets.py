c841d8f6856b37923fedd7a259ac9a83dad25a29
Igorello74 <sidneff.igor@ya.ru>
2022-07-13 23:25:36 +0500

    add choices.js assets to a local directory
    For stability

--- readersRecords/widgets.py
+++ readersRecords/widgets.py
@@ -4,12 +4,12 @@ from django import forms
 class ChoicesjsTextWidget(forms.widgets.Input):
     class Media:
         js = (
-            'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/scripts/choices.min.js',
+            'assets/choices.js/choices.min.js',
             'js/choices-js-widget.js'
         )
         css = {
             'all': (
-                'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css',
+                'assets/choices.js/choices.min.css',
             )
         }
 
