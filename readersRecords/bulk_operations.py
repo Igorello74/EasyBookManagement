f348db8f4ab236e344ef943f3b2305497a4e93ce
Igorello74 <sidneff.igor@ya.ru>
2022-07-22 23:17:02 +0500

    edit how xlsx files with missing cols are treated
    Now if a col is absent and it's presented in headers param,
    an error is raised

--- readersRecords/bulk_operations.py
+++ readersRecords/bulk_operations.py
@@ -6,64 +6,65 @@ from openpyxl import load_workbook
 from .models import Reader
 
 
-class NameNotFoundError(LookupError):
-    ...
+class ColumnNotFoundError(LookupError):
+    def __init__(self, missing_columns: list):
+        self.missing_columns = missing_columns
+
+        message = f"{', '.join(missing_columns)} not present in your sheet"
+        super().__init__(message)
 
 
-def import_from_xlsx(file, custom_headers: dict = {}):
-    """Import readers from a xlsx file (a file object must be passed)
+def import_from_xlsx(file, headers: dict = {}) -> int:
+    """Import readers from a xlsx (a file object must be passed)
 
-    If xlsx-table headers are different from default, you need to pass
-    them as a second arg (you can omit some headers to leave them default).
-    If any column (except for name) is not present, it's left blank in the model.
+    file: a file-like object open in binary mode
+
+    headers: a mapping of headers (see below) to columns (your custom column names)
+
+    If any column of those you provided is absent, a ColumnNotFoundError is raised.
     Note: headers are case insensitive.
 
-    Default headers:
-        name    - name,
-        role    - role (if you miss this column, role is set STUDENT)
-        group   - group (for example: "10а", "2б"; with no space between)
-        profile - profile
-        notes   - notes
-        first_lang  - lang1 (lang code according to ISO 639-1, like en, ru)
-        second_lang - lang2 (the same as lang1)
+    Returns num of readers imported.
+
+    Possible headers:
+        name, role, group, profile, notes, first_lang (ISO 639-1, like en, ru), second_lang
     """
+    POSSIBLE_HEADERS = ("name", "role", "group", "profile", "notes", "first_lang", "second_lang")
 
-    headers = {
-        "name": "name",
-        "role": "role",
-        "group": "group",
-        "profile": "profile",
-        "notes": "notes",
-        "first_lang": "lang1",
-        "second_lang": "lang2",
-    }  # TODO: maybe add books field
 
-    custom_headers = {k: v.lower() for k, v in custom_headers.items()}
-    headers.update(custom_headers)
+    headers = {k: v.lower() for k, v in headers.items() if k in POSSIBLE_HEADERS}
+    assert "name" in headers, "Name is required!"
     sheet = load_workbook(file, data_only=True).active
 
     # Make headers mapping
     temp_mapping = {}  # map custom headers to col indexes
-    for ind, row in enumerate(sheet.iter_cols(min_row=1, max_row=1, values_only=True)):
-        temp_mapping[row[0].lower()] = ind
+    for index, column in enumerate(sheet.iter_cols(min_row=1, max_row=1, values_only=True)):
+        temp_mapping[column[0].lower()] = index
     mapping = {}  # map standard keys to col indexes
-    for k, v in headers.items():
-        if v in temp_mapping:
-            mapping[k] = temp_mapping[v]
-    del temp_mapping, headers
+    missing_columns = []
+    for header, column_name in headers.items():
+        if column_name in temp_mapping:
+            mapping[header] = temp_mapping[column_name]
+        else:
+            missing_columns.append(column_name)
+        
+    if missing_columns:
+        raise ColumnNotFoundError(missing_columns)
 
     if "name" not in mapping:
-        raise NameNotFoundError(
-            "Student's name wasn't presented in your sheet"
-        )
+        raise ColumnNotFoundError("name", headers['name'])
 
     # Iterate through rows and save models
-    for row in sheet.iter_rows(min_row=2, values_only=True):
+    counter_saved = 0
+    for column in sheet.iter_rows(min_row=2, values_only=True):
         r = Reader()
-        for field, ind in mapping.items():
-            setattr(r, field, row[ind])
+        for field, index in mapping.items():
+            setattr(r, field, column[index])
 
         if "role" not in mapping:
             r.role = Reader.STUDENT
 
         r.save()
+        counter_saved += 1
+    
+    return counter_saved
