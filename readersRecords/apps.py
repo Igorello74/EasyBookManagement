3b0f097f7490ea2020a974f0c27c0463e447ceb1
Igorello74 <sidneff.igor@ya.ru>
2022-07-29 17:41:24 +0500

    add apps verbose names

--- readersRecords/apps.py
+++ readersRecords/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ReadersrecordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'readersRecords'
+    verbose_name = "Формуляры читателей"
