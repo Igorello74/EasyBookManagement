ae3f5df8177910a0200f84e33a00f771e20eb00a
Igorello <sidneff.igor@ya.ru>
2022-08-01 17:15:54 +0500

    create XlsxDictReader class
    Create XlsxDictReader (partly copied from csv.DictReader), so to make
    universal  import readers func. WIP on #31

--- readersRecords/bulk_operations.py
+++ readersRecords/bulk_operations.py
@@ -2,8 +2,64 @@
 such as import/export from/to json, csv, xlsx etc.
 """
 
+import csv
+from collections import namedtuple
+
 from openpyxl import load_workbook
+
 from .models import Reader
+
+
+class XlsxDictReader:
+    def __init__(self, f, fieldnames=None, restkey=None,
+                 restval=None, *args, **kwds):
+        self._fieldnames = fieldnames   # list of keys for the dict
+        self.restkey = restkey          # key to catch long rows
+        self.restval = restval          # default value for short rows
+        self.reader = load_workbook(
+            f, read_only=True, data_only=True, *args, **kwds
+        ).active.iter_rows(values_only=True)
+        self.line_num = 0
+    
+    
+
+    def __iter__(self):
+        return self
+
+    @property
+    def fieldnames(self):
+        if self._fieldnames is None:
+            try:
+                self._fieldnames = next(self.reader)
+            except StopIteration:
+                pass
+        self.line_num = 1
+        return self._fieldnames
+
+    @fieldnames.setter
+    def fieldnames(self, value):
+        self._fieldnames = value
+
+    def __next__(self):
+        if self.line_num == 0:
+            # Used only for its side effect.
+            self.fieldnames
+        row = next(self.reader)
+
+        # unlike the basic reader, we prefer not to return blanks,
+        # because we will typically wind up with a dict full of None
+        # values
+        while row == []:
+            row = next(self.reader)
+        d = dict(zip(self.fieldnames, row))
+        lf = len(self.fieldnames)
+        lr = len(row)
+        if lf < lr:
+            d[self.restkey] = row[lf:]
+        elif lf > lr:
+            for key in self.fieldnames[lr:]:
+                d[key] = self.restval
+        return d
 
 
 class ColumnNotFoundError(LookupError):
@@ -29,10 +85,10 @@ def import_from_xlsx(file, headers: dict = {}) -> int:
     Possible headers:
         name, role, group, profile, notes, first_lang (ISO 639-1, like en, ru), second_lang
     """
-    POSSIBLE_HEADERS = ("name", "role", "group", "profile", "notes", "first_lang", "second_lang")
-
-
-    headers = {k: v.lower() for k, v in headers.items() if k in POSSIBLE_HEADERS}
+    POSSIBLE_HEADERS = ("name", "role", "group", "profile",
+                        "notes", "first_lang", "second_lang")
+    headers = {k: v.lower()
+               for k, v in headers.items() if k in POSSIBLE_HEADERS}
     assert "name" in headers, "Name is required!"
     sheet = load_workbook(file, data_only=True).active
 
@@ -47,7 +103,7 @@ def import_from_xlsx(file, headers: dict = {}) -> int:
             mapping[header] = temp_mapping[column_name]
         else:
             missing_columns.append(column_name)
-        
+
     if missing_columns:
         raise ColumnNotFoundError(missing_columns)
 
@@ -67,5 +123,5 @@ def import_from_xlsx(file, headers: dict = {}) -> int:
 
         r.save()
         counter_saved += 1
-    
+
     return counter_saved
