fd6665c46812a86684177df3528042f75b236709
Igorello <sidneff.igor@ya.ru>
2022-08-25 00:42:17 +0500

    add UI fixes to purchaseRecords admin

--- purchaseRecords/admin.py
+++ purchaseRecords/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django import forms
 
 from . import models
 from .widgets import DateInput
 
+
+class BookPurchaseInline(admin.TabularInline):
+    model = models.BookPurchase
+    autocomplete_fields = ["book"]
+    formfield_overrides = {
+        models.models.TextField: {'widget': forms.Textarea(attrs={"rows": 1})}
+    }
+
+    class Media:
+        css = {"all": ("purchaseRecords/fix.css",)}
 
 @admin.register(models.BookInvoice)
 class BookInvoiceAdmin(admin.ModelAdmin):
@@ -11,6 +22,7 @@ class BookInvoiceAdmin(admin.ModelAdmin):
     }
 
     search_fields = ["custom_number", "number", "date"]
+    inlines = [BookPurchaseInline]
 
 
 @admin.register(models.BookPurchase)
