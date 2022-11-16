d77a5db42ca50bbd9dcff25e6a7a093d26de2fd2
root <root@biblio.malikula>
2022-11-16 10:09:38 +0000

    fix a bug about an invoice having no items
    There was a bug (like displaying error pages) when at least
    a single invoice with no items existed.

--- core/__init__.py
+++ core/__init__.py
@@ -2,5 +2,6 @@ from core.bulk_operations import BulkManager
 from core.factory import ObjectFactory
 
 def format_currency(val):
+    if val is None: return 0
     val = int(val)
     return f"{val:,}".replace(",", " ")
