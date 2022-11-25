2370536552d01258cabd7f72c81e80fad9a7ca0b
biblio.server <root@biblio.malikula>
2022-11-25 10:14:26 +0000

    change purchaseRecords name to Russian

--- purchaseRecords/apps.py
+++ purchaseRecords/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PurchaserecordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'purchaseRecords'
+    verbose_name = "Закупки"
