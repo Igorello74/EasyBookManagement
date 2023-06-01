653fb3540bb8327c7861307faf854fe185b7a104
Igorello74 <sidneff.igor@ya.ru>
2023-06-02 01:52:14 +0500

    add an export button to readers list
    Add an export button to readers list, also tune some css`s
    and modify url routes.

--- readersRecords/urls.py
+++ readersRecords/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = "readersRecords"
-urlpatterns = [
+admin_urlpatterns = [
+    path('import/', views.import_xlsx, name="readers-import"),
+    path('export/', views.export_xlsx, name="readers-export"),
+
 ]
