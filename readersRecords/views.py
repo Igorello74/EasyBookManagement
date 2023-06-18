b43319e57e324d14453b17daefa2599113440e37
Igorello74 <sidneff.igor@ya.ru>
2023-06-18 23:27:19 +0500

    add permission check to import-export views

--- readersRecords/views.py
+++ readersRecords/views.py
@@ -1,10 +1,10 @@
-
 import re
 from collections import defaultdict
 from datetime import datetime
 
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import permission_required
 from django.db.models import Count
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
@@ -19,46 +19,57 @@ from importExport.views import ExportView, ImportView
 from .models import Reader
 
 
-@method_decorator(staff_member_required, name="dispatch")
+@method_decorator(
+    [
+        staff_member_required,
+        permission_required("readersRecords.add_reader", raise_exception=True),
+    ],
+    name="dispatch",
+)
 class ReaderImportView(ImportView):
     model = Reader
     template_name = "readersRecords/import.html"
     headers_mapping = {
-        'name': 'имя',
-        'group': 'класс',
-        'profile': 'профиль',
-        'first_lang': 'язык 1',
-        'second_lang': 'язык 2',
-        'role': "роль"
+        "name": "имя",
+        "group": "класс",
+        "profile": "профиль",
+        "first_lang": "язык 1",
+        "second_lang": "язык 2",
+        "role": "роль",
     }
-    required_fields = ['name']
+    required_fields = ["name"]
     page_title = "Загрузить базу читателей"
 
 
 import_xlsx = ReaderImportView.as_view()
 
 
+@method_decorator(
+    [
+        staff_member_required,
+        permission_required("readersRecords.view_reader", raise_exception=True),
+    ],
+    name="dispatch",
+)
 class ReaderExportView(ExportView):
     model = Reader
     headers_mapping = {
-        'id': 'id',
-        'name': 'Имя',
-        'group': 'Класс',
-        'profile': 'Профиль',
-        'first_lang': 'Язык 1',
-        'second_lang': 'Язык 2',
-        'books': "Книги"
+        "id": "id",
+        "name": "Имя",
+        "group": "Класс",
+        "profile": "Профиль",
+        "first_lang": "Язык 1",
+        "second_lang": "Язык 2",
+        "books": "Книги",
     }
-    related_fields = {'books'}
+    related_fields = {"books"}
 
     @staticmethod
     def format_related(qs) -> str:
         result = []
         for count, i in enumerate(qs.select_related("book"), 1):
             author = Truncator(i.book.authors).words(1)
-            result.append(
-                f"{count}. {i.book.name} ({author}) — [{i.barcode}]"
-            )
+            result.append(f"{count}. {i.book.name} ({author}) — [{i.barcode}]")
 
         return "\n".join(result)
 
@@ -74,7 +85,7 @@ class defaultdict_recursed(defaultdict):
         super().__init__(defaultdict_recursed)
 
     def __getitem__(self, key):
-        if key in {"items", 'keys', 'values'}:
+        if key in {"items", "keys", "values"}:
             raise KeyError
         return super().__getitem__(key)
 
@@ -85,27 +96,31 @@ def get_item(dictionary, key):
 
 
 class ReadersUpdateGradeAction(View):
+    TO_DELETE = "К УДАЛЕНИЮ"
+
     def post(self, request, queryset=None, *args, **kwargs):
         if queryset is not None:
             queryset = queryset.annotate(Count("books"))
             readers = queryset.filter(role=Reader.STUDENT).values(
-                "id", "name", "group", "books__count")
+                "id", "name", "group", "books__count"
+            )
             readers_grouped = defaultdict_recursed()
             groups = set()
             for r in readers:
-                group_num, group_letter = re.match(
-                    "(\d+)(.+)", r['group']).groups()
+                group_num, group_letter = re.match("(\d+)(.+)", r["group"]).groups()
                 group_num = int(group_num)
                 groups.add((group_num, group_letter))
-                readers_grouped[group_num][r['group']][r['id']] = r
+                readers_grouped[group_num][r["group"]][r["id"]] = r
 
             new_groups = {}
             for group_num, group_letter in groups:
                 if group_num < settings.READERSRECORDS_MAX_GRADE:
-                    new_groups[f"{group_num}{group_letter}"] = f"{group_num+1}{group_letter}"
+                    new_groups[
+                        f"{group_num}{group_letter}"
+                    ] = f"{group_num+1}{group_letter}"
                 else:
-                    new_groups[f"{group_num}{group_letter}"] = "К УДАЛЕНИЮ"
-            
+                    new_groups[f"{group_num}{group_letter}"] = self.TO_DELETE
+
             readers_grouped = dict(sorted(readers_grouped.items()))
 
             return TemplateResponse(
@@ -114,15 +129,33 @@ class ReadersUpdateGradeAction(View):
                 {
                     "readers_grouped": readers_grouped,
                     "title": "Перевод учеников в следующий класс",
-                    'new_groups': new_groups
-                }
+                    "new_groups": new_groups,
+                },
             )
         else:
-            students = []
+            students_to_update = []
+            students_to_delete_ids = []
+
             for key, val in request.POST.items():
-                if key.startswith('student-'):
-                    students.append(Reader(id=key.removeprefix("student-"), group=val))
-            Reader.objects.bulk_update(students, ("group",))
+                if key.startswith("student-"):
+                    student_id = int(key.removeprefix("student-"))
+                    if val != self.TO_DELETE:
+                        students_to_update.append(Reader(id=student_id, group=val))
+                    else:
+                        students_to_delete_ids.append(student_id)
+
+            students_to_delete = Reader.objects.filter(id__in=students_to_delete_ids)
+
+            students_to_delete_with_books = students_to_delete.annotate(
+                Count("books")
+            ).filter(books__count__gt=0)
+
+            if students_to_delete_with_books.exists():
+                return HttpResponse(
+                    "Ты совершаешь ошибку. Нельзя удалять тех, на ком записаны книги!!!"
+                )
+            else:
+                Reader.objects.bulk_update(students_to_update, ("group",))
             return redirect("admin:readersRecords_reader_changelist")
 
     def get(self, request, *args, **kwargs):
