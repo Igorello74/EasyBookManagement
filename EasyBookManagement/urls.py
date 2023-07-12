7fa572d8da8c2c564b5faa8c53aba60a13f4e4f7
Igorello74 <sidneff.igor@ya.ru>
2023-07-12 21:35:44 +0500

    change the admin site url, header and title

--- EasyBookManagement/urls.py
+++ EasyBookManagement/urls.py
@@ -19,8 +19,12 @@ from django.urls import include, path
 import booksRecords.urls
 import readersRecords.urls
 
+admin.site.site_header = "Easy Book Management — автоматизированный учёт книг"
+admin.site.site_url = None
+admin.site.site_title = "EBM"
+admin.site.index_title = "Главная страница"
 urlpatterns = [
-    path('admin/readersRecords/reader/', include(readersRecords.urls.admin_urlpatterns)),
-    path('admin/', admin.site.urls),
+    path('readersRecords/reader/', include(readersRecords.urls.admin_urlpatterns)),
+    path('', admin.site.urls),
     path('books/', include(booksRecords.urls)),
 ]
